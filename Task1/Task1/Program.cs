d9ba69abc3fdd92d432aea072273de3410873c54
puruj <puru123456789@hotmail.com>
2023-02-25 22:52:41 -0500

    solution to task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Solution s = new Solution();
+
+int[] A = {1, 3, 6, 4, 1, 2 }; //Should return 5
+int[] B = {1, 2, 3};  //Should return 4
+int[] C = {-1,-3}; //Should return 1
+
+var example1 = s.solution(A);
+var example2 = s.solution(B);
+var example3 = s.solution(C);
+
+Console.WriteLine($"The result of example1 is {example1}.");
+Console.WriteLine($"The result of example2 is {example2}.");
+Console.WriteLine($"The result of example3 is {example3}.");
+
+
---
+++ Task1/Task1/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Solution
+{
+    /*
+     * Time complexity:  O(N), since we traverse the input array and where N is the length of the input array.
+     * Space complexity: O(1), since we modify the input array in place.
+     */
+    public int solution(int[] A)
+    {
+        if (A == null || A.Length == 0)
+        {
+            return 1;
+        }
+
+        int N = A.Length;
+
+        for (int i = 0; i < N; i++)
+        {
+            // Swap the elements to their corresponding positions
+            while (A[i] > 0 && A[i] <= N && A[A[i] - 1] != A[i])
+            {
+                int temp = A[A[i] - 1];
+                A[A[i] - 1] = A[i];
+                A[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            // find first index where value doesn't match corresponding index + 1
+            if (A[i] != i + 1)
+            {
+                return i + 1;
+            }
+        }
+
+        return N + 1;
+    }
+}
